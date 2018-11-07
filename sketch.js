13a2a1050c58acf3140d586d0401cb2d2fc06248
Heath <43174742+heathguy@users.noreply.github.com>
2018-11-07 10:28:51 -0500

    Update sketch.js
    Add digital clock to center of display

--- sketch.js
+++ sketch.js
@@ -2,26 +2,50 @@ let hourArc;
 let minArc;
 let secArc;
 
+let hourText;
+let minText;
+let secText;
+
+let timeText;
+
 function setup() {
   createCanvas(400, 400);
   hourArc = hour();
   minArc = minute();
   secArc = second();
 	
+	if(hourArc < 10) { hourText = "0" + hourArc; }
+	else { hourText = hourArc; }
+	if(minArc < 10) { minText = "0" + minArc; }
+	else { minText = minArc; }
+	if(secArc < 10) { secText = "0" + secArc; }
+	else { secText = secArc; }
+	timeText = hourText + ":" + minText + ":" + secText;
+	
 	//frameRate(1);
 	
   angleMode(DEGREES);	
-	
 }
 
 function draw() {
+	background(220);
+	
 	hourArc = hour();
 	minArc = minute();
 	secArc = second();
 	
+	if(hourArc < 10) { hourText = "0" + hourArc; }
+	else { hourText = hourArc; }
+	if(minArc < 10) { minText = "0" + minArc; }
+	else { minText = minArc; }
+	if(secArc < 10) { secText = "0" + secArc; }
+	else { secText = secArc; }
+	timeText = hourText + ":" + minText + ":" + secText;
+	
 	secArc = map(secArc,0,60,-90,270);
 	minArc = map(minArc,0,60,-90,270);
-	hourArc = map(hourArc,0,12,-90,270);
+	hourArc = map(hourArc,0,12,0,360);
+	hourArc = map(hourArc,0,360,-90,270);
 	
 	var sangle = secArc + 90;
 	secX = sin(sangle);
@@ -34,8 +58,6 @@ function draw() {
 	var hangle = hourArc + 90;
 	hourX = sin(hangle);
 	hourY = -cos(hangle);
-	
-  background(220);
 	
 	strokeWeight(5);
 	noFill();
@@ -53,8 +75,11 @@ function draw() {
 	// sec arc
 	stroke(255,0,0);
 	arc(width/2, height/2, 160, 160, -90, secArc);
-	line(width/2, height/2, (width/2)+80*secX, (height/2)+80*secY);
-
-	//console.log(hourArc);
-
+	line(width/2, height/2, ((width/2)+80*secX), ((height/2)+80*secY));
+	
+	strokeWeight(4);
+	fill(255);
+	stroke(0);
+	textSize(32);
+	text(timeText,width/2-65,height/2+10);
 }
